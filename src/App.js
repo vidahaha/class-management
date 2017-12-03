44bbbc1f946d6ed0387a7b4aa7060d7ca3bc81bc
vidahaha <937634115@qq.com>
2017-12-03 19:51:03 +0800

    make index

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">班级管理系统</h1>
+          <a className="App-login">请登录</a>
         </header>
         <div className="App-intro">
-          Hello world
+          <div className="intro-head">
+            <div className="head-info-1">
+              查看个人信息
+            </div>
+            <div className="head-info-2">
+              查看班级成员
+            </div>
+            <div className="head-info-3">
+              查看已开课程
+            </div>
+          </div>
+          <div className="intro-main">
+
+          </div>
         </div>
         <footer className="App-footer">
-          <img src={logo} className="App-logo" alt="logo" />
+          vidahaha
         </footer>
       </div>
     );
