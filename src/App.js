b1133cb9b0980f00ebb864ce2ce6a934733261fc
vidahaha <937634115@qq.com>
2017-12-04 20:19:00 +0800

    add event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
 import logo from './logo.svg';
 import './css/App.css';
 
 class App extends Component {
+  // componentDidMount() {
+  //   const info1 = this.refs.info1;
+  //   const info1Dom = ReactDOM.findDOMNode( info1 );
+  //   console.log( getComputedStyle(info1Dom).width )
+  // }
+  largerImg( event ) {
+    console.log( getComputedStyle(event.target).width )
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,13 +23,13 @@ class App extends Component {
         </header>
         <div className="App-intro">
           <div className="intro-head">
-            <div className="head-info-1">
+            <div className="head-info-1" onClick={this.largerImg}>
               查看个人信息
             </div>
-            <div className="head-info-2">
+            <div className="head-info-2" onClick={this.largerImg}>
               查看班级成员
             </div>
-            <div className="head-info-3">
+            <div className="head-info-3" onClick={this.largerImg}>
               查看已开课程
             </div>
           </div>
