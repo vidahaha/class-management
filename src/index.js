3802f2aa0563c0b2f4447d0f6ca95cc7d11fe7aa
vidahaha <937634115@qq.com>
2017-12-11 23:04:29 +0800

    add react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+  } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+	<Router>
+		<Route path="/" component={App}/>
+  	</Router>
+), document.getElementById('root'));
+
+//ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
